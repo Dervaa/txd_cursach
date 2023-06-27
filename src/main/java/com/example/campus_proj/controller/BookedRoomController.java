202ea32dd5be2cf43e29ec11635015c8b3ff35a6
Createl15 <zlata2003@inbox.ru>
2023-06-28 02:27:37 +0300

    Update controller

--- src/main/java/com/example/campus_proj/controller/BookedRoomController.java
+++ src/main/java/com/example/campus_proj/controller/BookedRoomController.java
@@ -1,65 +1,58 @@
 package com.example.campus_proj.controller;
-import com.example.campus_proj.Entity.Booked_room;
-import com.example.campus_proj.Entity.Reference_room;
-import com.example.campus_proj.Service.Booked_Rooms_Service;
-import com.example.campus_proj.Service.Reference_room_Service;
+import com.example.campus_proj.Entity.BookedRoom;
+import com.example.campus_proj.Service.BookedRoomsService;
+import com.example.campus_proj.Service.ReferenceRoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Optional;
 
 @Controller
 @RequestMapping("/booked_rooms")
 public class BookedRoomController {
     @Autowired
-     Booked_Rooms_Service bookedRoomsService;
+    BookedRoomsService bookedRoomsService;
     @Autowired
-    Reference_room_Service referenceRoomService;
-
-    @Autowired
-    public BookedRoomController(Booked_Rooms_Service bookedRoomsService) {
-        this.bookedRoomsService = bookedRoomsService;
-    }
-
-    /*@GetMapping
-    public String getAlltheBookedRooms(Model model) {
-        List<Booked_room> bookedRooms = bookedRoomsService.getAllBookedRooms();
-        model.addAttribute("bookedRooms", bookedRooms);
-        return "booked_rooms";
-    }*/
+    ReferenceRoomService referenceRoomService;
     @GetMapping
     public String getAll(Model model) {
-        model.addAttribute("bookedRooms", bookedRoomsService.getAllBookedRooms());
-        model.addAttribute("referenceRooms", referenceRoomService.getAllReferenceRooms());
+        model.addAttribute("bookedRooms", bookedRoomsService.getAll());
+        model.addAttribute("referenceRooms", referenceRoomService.getAll());
         return "booked_rooms";
     }
-
-    @PostMapping("booked_rooms/delete/{id}")
-    public String deleteBooked_roomById(@PathVariable Long id) {
-        bookedRoomsService.deleteBookedRoomById(id);
+    @GetMapping("/{id}")
+    public String getById(@PathVariable("id") Long id, Model model) {
+        BookedRoom bookedRoom = bookedRoomsService.getById(id);
+        model.addAttribute("bookedRoom", bookedRoom);
+        return "bookedRoom-details";
+    }
+    @PostMapping("/create")
+    public String create(@ModelAttribute BookedRoom bookedRoom) {
+        bookedRoomsService.create(bookedRoom);
+        return "redirect:/booked_rooms";
+    }
+    @PostMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Long id) {
+        bookedRoomsService.delete(id);
+        return "redirect:/booked_rooms";
+    }
+    @PostMapping("/save")
+    public String save(@ModelAttribute("bookedRoom") BookedRoom bookedRoom) {
+        bookedRoomsService.update(bookedRoom);
         return "redirect:/booked_rooms";
     }
     @GetMapping("/edit/{id}")
-    public String editBooked_roomById(@PathVariable Long id, Model model)
-    {
-        Optional<Booked_room> booked_room = bookedRoomsService.getById(id);
-        model.addAttribute("booked_room", booked_room);
-        return "booked_rooms";
+    public String getSong(@PathVariable("id") Long id, Model model) {
+        BookedRoom bookedRoom = bookedRoomsService.getById(id);
+        model.addAttribute("bookedRoom", bookedRoom);
+        model.addAttribute("referenceRooms", referenceRoomService.getAll());
+        return "editBookedRoom";
     }
     @PostMapping("/edit/{id}")
-    public String editBooked_roomById(@PathVariable Long id,
-                                      @RequestParam("capacity") Integer capacity,
-                                      @RequestParam("cost") Integer cost,
-                                      @RequestParam("occupationStatus") boolean occupation_status, Model model)
-    {
-        Optional<Booked_room> booked_room = bookedRoomsService.getById(id);
-        model.addAttribute("booked_room", booked_room);
-        return "booked_rooms";
+    public String editSong(@ModelAttribute BookedRoom bookedRoom) {
+        bookedRoomsService.update(bookedRoom);
+        return "redirect:/booked_rooms";
     }
 }
-
-
-
